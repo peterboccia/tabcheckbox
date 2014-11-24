ea240ffaa30f698ecb01a25f0c381b60c0bfaf63
Peter <peterboccia88@gmail.com>
2014-11-24 14:23:20 +0100

    First script file

---
+++ jquery.tabbedcheckbox.js
@@ -0,0 +1,75 @@
+// Jquery Plugin Tabbed Combo Box
+// A plugin to render checbox as tabbed button
+// Version 0.1 - 24 Nov 2014
+// by Peter Boccia
+// peterboccia88@gmail.com
+
+(function ($) {
+
+    $.tabComboBox = function (element, options) {
+
+        var defaults = {
+            activeClass: 'active',
+            inactiveClass: 'inactive',
+            attributeLabel: 'data-text',
+            onChange: function () { }
+        }
+
+        var plugin = this;
+
+        plugin.settings = {}
+
+        var $element = $(element);
+        var span;
+
+        plugin.init = function () {
+            plugin.settings = $.extend({}, defaults, options);
+
+            $element.hide();
+
+            span = $('<span />')
+                .addClass('tabbedCheckbox')
+                .text($element.attr(plugin.settings.attributeLabel));
+
+            if ($element.is(':checked') || $element.val() == 'true') {
+                plugin.select();
+            }
+            else {
+                plugin.deselect();
+            }
+
+            span.click(function (e) {
+                if ($element.val() == 'true') {
+                    plugin.deselect();
+                }
+                else {
+                    plugin.select();
+                }
+            });
+
+            span.insertAfter($element);
+        }
+
+        plugin.select = function () {
+            span.removeClass(plugin.settings.inactiveClass).addClass(plugin.settings.activeClass);
+            $element.val(true);
+        }
+
+        plugin.deselect = function () {
+            span.removeClass(plugin.settings.activeClass).addClass(plugin.settings.inactiveClass);
+            $element.val(false);
+        }
+
+        plugin.init();
+    }
+
+    $.fn.tabComboBox = function (options) {
+        return this.each(function () {
+            if (undefined == $(this).data('tabComboBox')) {
+                var plugin = new $.tabComboBox(this, options);
+                $(this).data('tabComboBox', plugin);
+            }
+        });
+    }
+
+})(jQuery);
